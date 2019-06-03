3d427d9ffd345538ce794cc67777fc984cde2db1
nhuanh9 <49300627+nhuanh9@users.noreply.github.com>
2019-06-03 14:59:17 +0700

    .

---
+++ ex1.js
@@ -0,0 +1,7 @@
+let ly,hoa,sinh;
+ly = eval(prompt("Nhap diem ly:"));
+hoa = eval(prompt("Nhap diem hoa:"));
+sinh = eval(prompt("Nhap diem sinh:"));
+let Tong = (ly+hoa+sinh);
+document.write("Diem TB:"+Tong/3);
+document.write("<br>Tong Diem:"+Tong);
